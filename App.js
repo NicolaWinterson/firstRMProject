506beccd1b345edf4cd03f887483329b076257e7
Nicola Winterson <nicola.l.winterson@gmail.com>
2021-03-31 12:04:42 +0200

    implemented api search

--- App.js
+++ App.js
@@ -1,28 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from "./SearchBar"
 import Loading from "./Loading"
+import {API_KEY} from '@env'
+import axios from 'axios';
 import { StyleSheet, View, Text, TextInput, Button, Modal, ActivityIndicator } from 'react-native'
 
 
 export default function App() {
 
-  const onPressButton = () => {
+  /* const onPressButton = () => {
     alert('You pressed the button!')
+  } */
+
+  const [plant, setPlant] = useState({});
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("coconut");
+
+  useEffect(() => {
+    
+    const getPlant = async () => {
+      try {
+        const URL = `https://trefle.io/api/v1/plants/search?token=${API_KEY}&q=${query}`;
+        const response = await fetch(URL, {
+          mode: "cors"
+        });
+        console.log("response is:" + response);
+        const data = await response.json();
+        var stringifiedData = JSON.stringify(data);
+        console.log("response done, let's read the json" + data)
+        console.log("that data stringified" + stringifiedData);
+        setPlant(data);
+      } catch (error) {
+        setError(error);
+        console.log("it is broken");
+        console.error(error.message);
+      }
+    };
+
+    getPlant();
+    console.log("effect has been run");
+  }, [query]);
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  if (error) {
+    return <div style={{ color: "red" }}>{error.message}</div>;
   }
-  
+
+
   return (
     <View style={styles.container}>
       <Loading />
 
       <View>
-        <Text style={styles.dummyText}>Pizza Search</Text>
+        <Text style={styles.dummyText}>Plant Search</Text>
       </View>
-      <SearchBar />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Search plants..."
+        onChangeText={updateSearch}
+        defaultValue={search}
+      />
       <Button
+        onPress={getSearch}
+        title="Search"
+      />
+
+      {/* <SearchBar /> */}
+{/*       <Button
         onPress={onPressButton}
         title="Press Me"
         color="#841584"
-      />
+      /> */}
     </View>
   )
 }
@@ -33,6 +92,7 @@ const styles = StyleSheet.create({
     backgroundColor: "pink",
   },
   dummyText: {
+    top:'5%',
     color: 'green',
     fontWeight: 'bold',
     fontSize: 30,
--- Loading.js
+++ Loading.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 import { StyleSheet, View, Text, TextInput, Button, Modal, ActivityIndicator } from 'react-native'
 
 export default function Loading() {
